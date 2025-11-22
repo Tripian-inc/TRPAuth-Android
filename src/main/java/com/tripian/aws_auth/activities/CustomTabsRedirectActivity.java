ec033f43f2659c9bcc0b4e7d927fb58ce3f49c83
cmcygz <cem@tripian.com>
2025-11-22 03:23:58 +0300

    Initial TRPAuth Android library

---
+++ src/main/java/com/tripian/aws_auth/activities/CustomTabsRedirectActivity.java
@@ -0,0 +1,22 @@
+package com.tripian.aws_auth.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Handles auth redirect for sign-in and sign-out.
+ *
+ * If cognitoauth module is being used in conjunction with AWSMobileClient, then use
+ * com.amazonaws.mobile.client.activities.HostedUIRedirectActivity instead of this.
+ */
+public final class CustomTabsRedirectActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceBundle) {
+        super.onCreate(savedInstanceBundle);
+
+        // This ensures that the browser tab is removed from the back stack.
+        startActivity(CustomTabsManagerActivity.createResponseHandlingIntent(
+                this, getIntent().getData()));
+        finish();
+    }
+}
